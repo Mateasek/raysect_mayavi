eafafaa44538d45789040bcff9a1429fb2a7c50f
mattngc <matthewncarr@gmail.com>
2019-04-20 17:38:14 +0100

    Added a KDTree to the algorithm based on triangle centres.

--- raysect_mayavi/primitives/mesh_csg.pyx
+++ raysect_mayavi/primitives/mesh_csg.pyx
@@ -1,6 +1,6 @@
 
 import numpy as np
-from scipy.spatial import Delaunay
+from scipy.spatial import Delaunay, KDTree
 
 from raysect.core cimport Point3D, translate, rotate_basis
 
@@ -60,24 +60,43 @@ cpdef Mesh perform_mesh_csg(Mesh mesh_1, Mesh mesh_2, CSG_Operator operator):
     cdef:
         tuple result
 
+    # extract vertex and triangle data for mesh 1
     m1_vertices = mesh_1.data.vertices.copy()
     m1_triangles = mesh_1.data.triangles.copy()
     n_m1_vertices = m1_vertices.shape[0]
     n_m1_triangles = m1_triangles.shape[0]
 
+    # calculate triangle centres and KDtree for mesh 1
+    m1_tri_centres = np.zeros(m1_triangles.shape)
+    for m1_tri_id in range(n_m1_triangles):
+        v1, v2, v3 = m1_triangles[m1_tri_id]
+        u1x, u1y, u1z = m1_vertices[v1]
+        u2x, u2y, u2z = m1_vertices[v2]
+        u3x, u3y, u3z = m1_vertices[v3]
+        m1_tri_centres[m1_tri_id, :] = (u1x + u2x + u3x) / 3, (u1y + u2y + u3y) / 3, (u1z + u2z + u3z) / 3
+    m1_kdtree = KDTree(m1_tri_centres)
+
+    # extract vertex and triangle data for mesh 2
     m2_vertices = mesh_2.data.vertices.copy()
     m2_triangles = mesh_2.data.triangles.copy()
     n_m2_vertices = m2_vertices.shape[0]
     n_m2_triangles = m2_triangles.shape[0]
 
+    # calculate triangle centres and KDtree for mesh 2
+    m2_tri_centres = np.zeros(m2_triangles.shape)
+    for m2_tri_id in range(n_m2_triangles):
+        v1, v2, v3 = m2_triangles[m2_tri_id]
+        v1x, v1y, v1z = m2_vertices[v1]
+        v2x, v2y, v2z = m2_vertices[v2]
+        v3x, v3y, v3z = m2_vertices[v3]
+        m2_tri_centres[m2_tri_id, :] = (v1x + v2x + v3x) / 3, (v1y + v2y + v3y) / 3, (v1z + v2z + v3z) / 3
+    m2_kdtree = KDTree(m2_tri_centres)
 
     ####################################################################################################################
     # FIND ALL INTERSECTIONS AND SIGNED DISTANCES
 
     m1_intersects = np.full((n_m1_triangles), False)
     m1_intersections = {}
-    m1m2_distance = np.empty(n_m1_triangles)
-    m1m2_distance[:] = 1E999
     m1_from_m2_signed_distance = np.empty(n_m1_triangles)
     m1_from_m2_signed_distance[:] = 1E999
 
@@ -87,11 +106,10 @@ cpdef Mesh perform_mesh_csg(Mesh mesh_1, Mesh mesh_2, CSG_Operator operator):
     m2m1_distance[:] = 1E999
     m2_from_m1_signed_distance = np.empty(n_m2_triangles)
     m2_from_m1_signed_distance[:] = 1E999
-    m2_pair = np.zeros(n_m1_triangles)  # TODO - still needed?
-
 
     for m1_tri_id in range(n_m1_triangles):
 
+        # unpack vertices for triangle in mesh 1
         v1, v2, v3 = m1_triangles[m1_tri_id]
         u1x, u1y, u1z = m1_vertices[v1]
         u1 = Point3D(u1x, u1y, u1z)
@@ -99,11 +117,17 @@ cpdef Mesh perform_mesh_csg(Mesh mesh_1, Mesh mesh_2, CSG_Operator operator):
         u2 = Point3D(u2x, u2y, u2z)
         u3x, u3y, u3z = m1_vertices[v3]
         u3 = Point3D(u3x, u3y, u3z)
-        uc = Point3D((u1x + u2x + u3x) / 3, (u1y + u2y + u3y) / 3, (u1z + u2z + u3z) / 3)
+        uc = Point3D(m1_tri_centres[m1_tri_id, 0], m1_tri_centres[m1_tri_id, 1], m1_tri_centres[m1_tri_id, 2])
         n_pi1 = u1.vector_to(u2).cross(u1.vector_to(u3)).normalise()  # normal vector of plane 1
+        tri1_radius = max(uc.distance_to(u1), uc.distance_to(u2), uc.distance_to(u3))
 
-        for m2_tri_id in range(n_m2_triangles):
+        # find all mesh 2 triangles within radius r of mesh 1
+        m2_candidates = m2_kdtree.query_ball_point([uc.x, uc.y, uc.z], tri1_radius)
 
+        m1m2_distance = 1E999  # temp variable
+        for m2_tri_id in m2_candidates:
+
+            # unpack vertices for triangle in mesh 2
             v1, v2, v3 = m2_triangles[m2_tri_id]
             v1x, v1y, v1z = m2_vertices[v1]
             v1 = Point3D(v1x, v1y, v1z)
@@ -111,7 +135,7 @@ cpdef Mesh perform_mesh_csg(Mesh mesh_1, Mesh mesh_2, CSG_Operator operator):
             v2 = Point3D(v2x, v2y, v2z)
             v3x, v3y, v3z = m2_vertices[v3]
             v3 = Point3D(v3x, v3y, v3z)
-            vc = Point3D((v1x + v2x + v3x) / 3, (v1y + v2y + v3y) / 3, (v1z + v2z + v3z) / 3)
+            vc = Point3D(m2_tri_centres[m2_tri_id, 0], m2_tri_centres[m2_tri_id, 1], m2_tri_centres[m2_tri_id, 2])
             n_pi2 = v1.vector_to(v2).cross(v1.vector_to(v3)).normalise()  # normal vector of plane 2
 
             uv_distance = uc.distance_to(vc)
@@ -142,19 +166,64 @@ cpdef Mesh perform_mesh_csg(Mesh mesh_1, Mesh mesh_2, CSG_Operator operator):
                     m2_intersections[m2_tri_id] = [(m1_tri_id, intersect_pt_1, intersect_pt_2)]
 
             else:
-                if uv_distance < m1m2_distance[m1_tri_id]:
-                    m1m2_distance[m1_tri_id] = uv_distance
+                if uv_distance < m1m2_distance:
+                    m1m2_distance = uv_distance
                     m1_from_m2_signed_distance[m1_tri_id] = d_u_from_v
 
-                if uv_distance < m2m1_distance[m2_tri_id]:
-                    m2m1_distance[m2_tri_id] = uv_distance
-                    m2_from_m1_signed_distance[m2_tri_id] = d_v_from_u
+        if len(m2_candidates) == 0:
+
+            # query closest triangle in mesh 2
+            closest_m2_tri = m2_kdtree.query([uc.x, uc.y, uc.z])[1]
+
+            # unpack vertices for closest triangle in mesh 2
+            v1, v2, v3 = m2_triangles[closest_m2_tri]
+            v1x, v1y, v1z = m2_vertices[v1]
+            v1 = Point3D(v1x, v1y, v1z)
+            v2x, v2y, v2z = m2_vertices[v2]
+            v2 = Point3D(v2x, v2y, v2z)
+            v3x, v3y, v3z = m2_vertices[v3]
+            v3 = Point3D(v3x, v3y, v3z)
+            vc = Point3D(m2_tri_centres[closest_m2_tri, 0], m2_tri_centres[closest_m2_tri, 1], m2_tri_centres[closest_m2_tri, 2])
+            n_pi2 = v1.vector_to(v2).cross(v1.vector_to(v3)).normalise()  # normal vector of plane 2
+
+            d_u_from_v = -n_pi2.dot(uc.vector_to(vc))
+            m1_from_m2_signed_distance[m1_tri_id] = d_u_from_v
+
+
+    for m2_tri_id in range(n_m2_triangles):
+
+        # unpack vertices for triangle in mesh 2
+        v1, v2, v3 = m2_triangles[m2_tri_id]
+        v1x, v1y, v1z = m2_vertices[v1]
+        v1 = Point3D(v1x, v1y, v1z)
+        v2x, v2y, v2z = m2_vertices[v2]
+        v2 = Point3D(v2x, v2y, v2z)
+        v3x, v3y, v3z = m2_vertices[v3]
+        v3 = Point3D(v3x, v3y, v3z)
+        vc = Point3D(m2_tri_centres[m2_tri_id, 0], m2_tri_centres[m2_tri_id, 1], m2_tri_centres[m2_tri_id, 2])
+        n_pi2 = v1.vector_to(v2).cross(v1.vector_to(v3)).normalise()  # normal vector of plane 2
+
+        # query closest triangle in mesh 1
+        closest_m1_tri = m1_kdtree.query([vc.x, vc.y, vc.z])[1]
+
+        # unpack vertices for closest triangle in mesh 1
+        v1, v2, v3 = m1_triangles[closest_m1_tri]
+        u1x, u1y, u1z = m1_vertices[v1]
+        u1 = Point3D(u1x, u1y, u1z)
+        u2x, u2y, u2z = m1_vertices[v2]
+        u2 = Point3D(u2x, u2y, u2z)
+        u3x, u3y, u3z = m1_vertices[v3]
+        u3 = Point3D(u3x, u3y, u3z)
+        uc = Point3D(m1_tri_centres[closest_m1_tri, 0], m1_tri_centres[closest_m1_tri, 1], m1_tri_centres[closest_m1_tri, 2])
+        n_pi1 = u1.vector_to(u2).cross(u1.vector_to(u3)).normalise()  # normal vector of plane 1
+
+        d_v_from_u = -n_pi1.dot(vc.vector_to(uc))
+        m2_from_m1_signed_distance[m2_tri_id] = d_v_from_u
 
 
     ####################################################################################################################
     # ADD TRIANGLES THAT DON"T NEED TO BE SPLIT
 
-    # TODO - is this section being repeated? Maybe move to end of code
 
     combined_vertices = m1_vertices.tolist()
     combined_vertices.extend(m2_vertices.tolist())
