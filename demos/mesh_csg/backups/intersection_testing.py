dfad8905e9e6453e7b492f00b47203e4e942e46c
mattngc <matthewncarr@gmail.com>
2019-04-21 16:15:24 +0100

    Adding extra intersection test cases.

--- demos/mesh_csg/backups/intersection_testing.py
+++ demos/mesh_csg/backups/intersection_testing.py
@@ -2,7 +2,7 @@
 import numpy as np
 from mayavi import mlab
 from raysect.core import Point3D
-from raysect_mayavi.primitives.triangle import triangle3d_intersects_triangle3d
+from raysect_mayavi.primitives.triangle import Triangle, triangle3d_intersects_triangle3d
 
 
 # u1 = Point3D(0.0, 0.0, 1.0)
@@ -37,14 +37,26 @@ from raysect_mayavi.primitives.triangle import triangle3d_intersects_triangle3d
 # v2 = Point3D(0.6000000238418579, 0.6000000238418579, 0.6000000238418579)
 # v3 = Point3D(1.600000023841858, 0.6000000238418579, 0.6000000238418579)
 
-u1 = Point3D(0.0, 1.0, 0.0)
-u2 = Point3D(0.0, 0.0, 0.0)
-u3 = Point3D(0.0, 1.0, 1.0)
+# u1 = Point3D(0.0, 1.0, 0.0)
+# u2 = Point3D(0.0, 0.0, 0.0)
+# u3 = Point3D(0.0, 1.0, 1.0)
+#
+# v1 = Point3D(0.6000000238418579, 1.600000023841858, 1.600000023841858)
+# v2 = Point3D(0.6000000238418579, 0.6000000238418579, 1.600000023841858)
+# v3 = Point3D(1.600000023841858, 0.6000000238418579, 1.600000023841858)
 
-v1 = Point3D(0.6000000238418579, 1.600000023841858, 1.600000023841858)
-v2 = Point3D(0.6000000238418579, 0.6000000238418579, 1.600000023841858)
-v3 = Point3D(1.600000023841858, 0.6000000238418579, 1.600000023841858)
+u1 = Point3D(0.2628655433654785, 0.4253253936767578, 0.0)
+u2 = Point3D(0.21694427728652954, 0.4313342273235321, 0.12994596362113953)
+u3 = Point3D(0.34689024090766907, 0.35102322697639465, 0.08031101524829865)
 
+v1 = Point3D(0.30000001192092896, -0.5, 0.30000001192092896)
+v2 = Point3D(0.30000001192092896, 0.5, -0.699999988079071)
+v3 = Point3D(0.30000001192092896, 0.5, 0.30000001192092896)
+
+t1 = Triangle(u1.x, u1.y, u1.z, u2.x, u2.y, u2.z, u3.x, u3.y, u3.z)
+t2 = Triangle(v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, v3.x, v3.y, v3.z)
+
+result = triangle3d_intersects_triangle3d(t1, t2)
 
 dx = np.array([u1.x, u2.x, u3.x])
 dy = np.array([u1.y, u2.y, u3.y])
@@ -78,8 +90,7 @@ end = t2_cp + n_pi2.normalise() * 0.5
 mlab.plot3d([t2_cp.x, end.x], [t2_cp.y, end.y], [t2_cp.z, end.z], tube_radius=0.005)
 
 
-result = triangle3d_intersects_triangle3d(u1.x, u1.y, u1.z, u2.x, u2.y, u2.z, u3.x, u3.y, u3.z,
-                                          v1.x, v1.y, v1.z, v2.x, v2.y, v2.z, v3.x, v3.y, v3.z)
-
-
+if result[0]:
+    t2_point, t3_point = result[1], result[2]
+    mlab.plot3d([t2_point.x, t3_point.x], [t2_point.y, t3_point.y], [t2_point.z, t3_point.z], tube_radius=0.005, color=(0, 0, 0))
 
