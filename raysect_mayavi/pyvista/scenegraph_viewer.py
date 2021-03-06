7e839dd9704574cef0617ad1ba887619f66cf98b
Vladislav Neverov <vs-never@hotmail.com>
2021-03-06 14:32:12 +0300

    Fixed pyvista visualise_scenegraph.

--- raysect_mayavi/pyvista/scenegraph_viewer.py
+++ raysect_mayavi/pyvista/scenegraph_viewer.py
@@ -2,24 +2,22 @@ import pyvista as pv
 pv.rcParams['use_ipyvtk'] = True
 
 from raysect_mayavi.primitives.parse_nodes import parse_nodes
-from raysect_mayavi.mayavi.parse import parse_sources
+from raysect_mayavi.pyvista.parse import parse_sources
 
 
 def visualise_scenegraph(world, plotter=None, show_axes=False, axes_length=1):
     """
     Automatic visualisation of the raysect scenegraph.
-    :param world: Instance of the Raysect World 
+    :param world: Instance of the Raysect World
     """
 
-    if plotter is None:
-        figure = pv.Plotter(window_size=(1024, 768))
+    plotter = plotter or pv.Plotter(window_size=(1024, 768))
 
     sources = parse_nodes(world)
     visualisers = parse_sources(sources)
 
     for _, visualiser in visualisers.items():
 
-        visualiser.plot(figure)
-
+        visualiser.plot(plotter)
 
     return plotter
