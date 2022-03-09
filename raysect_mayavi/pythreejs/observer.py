0ea6776e97309c7a8377c649f7edf7e9e7bd6faa
MatejTomes <tomes@ipp.cas.cz>
2022-03-09 17:05:19 +0100

    Add pythreejs visualisation

---
+++ raysect_mayavi/pythreejs/observer.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from raysect_mayavi.primitives.observer import ObserverSource
+from raysect_mayavi.pythreejs.visualiser import PythreejsVisualiser
+
+from pythreejs import ArrowHelper, Vector3
+
+
+class ObserverVisualiser(PythreejsVisualiser):
+    """
+    Class for graphical representation of the Raysect observers.
+    :param raysect_object: Raysect Observer instance
+    """
+
+    def __init__(self, source):
+        
+        self.length = 1e-2
+        super().__init__(source)
+
+    def _init_object_kwargs(self):
+       self.plot_kwargs = {}
+       self.plot_kwargs["headLength"] = 0.2 * self.length 
+       self.plot_kwargs["headWidth"] = 0.2 * self.length
+       self.plot_kwargs["color"] = "green"
+    
+    @property
+    def length(self):
+        return self._length
+    
+    @length.setter
+    def length(self, value):
+
+        if not value > 0:
+            raise ValueError("value has to be larger than 0")
+        
+        self._length = value
+        
+
+    def set_source(self, source):
+
+        if not isinstance(source, ObserverSource):
+            raise TypeError("source has to be instance of SourceBase.")
+        
+        self._source = source
+    
+    def get_data_object(self):
+        origin = self._source.origin_root
+        origin = (origin.x, origin.y, origin.z)
+        direction = self.length * self._source.direction_root.normalise()
+        direction= (direction.x, direction.y, direction.z)
+        
+        return ArrowHelper(dir=direction, origin=origin, length=self.length, **self.plot_kwargs)
+
+    def _add_object(self, plotter):
+
+        mesh = self.get_data_object()
+
+        plotter.scene.add(mesh)
+
---
+++ raysect_mayavi/pythreejs/parse.py
@@ -0,0 +1,33 @@
+
+from raysect_mayavi.primitives.source import SourceBase, TriangularMeshSource
+from raysect_mayavi.primitives.ray import LoggingRaySource
+from raysect_mayavi.primitives.observer import ObserverSource
+
+from raysect_mayavi.pythreejs.mesh import TriangularMeshVisualiser
+from raysect_mayavi.pythreejs.ray import LoggingRayVisualiser
+from raysect_mayavi.pythreejs.observer import ObserverVisualiser
+
+
+def parse_sources(sources):
+    
+    visualisers = {}
+    
+    for key, source in sources.items():
+        visualisers[key] = assign_visualiser(source)
+
+    
+    return visualisers
+
+def assign_visualiser(source):
+    
+    if not isinstance(source, SourceBase):
+        raise TypeError("sources items has to be of type SourceBase.")
+    if isinstance(source, TriangularMeshSource):
+        return TriangularMeshVisualiser(source)
+    elif isinstance(source, LoggingRaySource):
+        return LoggingRayVisualiser(source)
+    elif isinstance(source, ObserverSource):
+        return ObserverVisualiser(source)
+    else:
+        raise TypeError("Source type not recognised")
+    
